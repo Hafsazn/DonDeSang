0743d15bf4564a6635d0a26dbdfdbf36a9a74ddf
Hafsazn <36314849+Hafsazn@users.noreply.github.com>
2018-05-23 15:20:01 +0000

    Add files via upload

--- ajoutplanning.php
+++ ajoutplanning.php
@@ -9,7 +9,7 @@
 
    
 if(isset($_POST['Ajouter'])) {
-   $id = htmlspecialchars($_POST['Id_planning']);
+   $id = htmlspecialchars($_POST['Id_Planning']);
    $personnel = htmlspecialchars($_POST['Personnel']);
    $salle = htmlspecialchars($_POST['Salle']);  
    $type = htmlspecialchars($_POST['Type']); 
@@ -17,14 +17,14 @@ if(isset($_POST['Ajouter'])) {
    $heurede = htmlspecialchars($_POST['Heure_de']);
    $heurea = htmlspecialchars($_POST['Heure_a']);
   
-   if(!empty($_POST['Id_planning']) AND !empty($_POST['Personnel']) AND !empty($_POST['Salle']) AND !empty($_POST['Type']) AND !empty($_POST['Date']) AND !empty($_POST['Heure_de']) AND !empty($_POST['Heure_a'])) {
+   if(!empty($_POST['Personnel']) AND !empty($_POST['Salle']) AND !empty($_POST['Type']) AND !empty($_POST['Date']) AND !empty($_POST['Heure_de']) AND !empty($_POST['Heure_a'])) {
             
                     include ('connexionBD.php');
             try{
                     // echo "hello";
                      //$bdd = new PDO('mysql:host=localhost;dbname = espace membre', 'root', '');
                  
-                     $query = "INSERT INTO planning (Id_planning, Personnel, Salle, Type, Date, Heure_de, Heure_a) VALUES ('$id', '$personnel', '$salle', '$type', '$date', '$heurede', '$heurea')";
+                     $query = "INSERT INTO planning (Personnel, Salle, Type, Date, Heure_de, Heure_a) VALUES ('$personnel', '$salle', '$type', '$date', '$heurede', '$heurea')";
                      //$query = "INSERT INTO donneur (Non, Prenon, CIN, Email, Mdp) VALUES ('zidani', 'hafid', 'HA5678', 'hafid@z.en', '123456')";
                      $bdd->exec($query);
                      echo "Ajout avec succes";
